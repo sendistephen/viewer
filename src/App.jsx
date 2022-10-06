a0f4ef55803abfb62cf9d29703568ede278b46b0
sendistephen <sendi.steve2@gmail.com>
2022-10-06 18:39:59 +0300

    WIP: Add sidebar for large screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { home } from "./assets";
+import { Navbar, SidebarLg, Trending } from "./components";
+import Home from "./components/Home";
 
 function App() {
 	return (
-		<div className="">
-			<h1 className="text-4xl text-green-500">Hello Viewer</h1>
-		</div>
+		<BrowserRouter>
+			<div className="flex h-screen overflow-hidden">
+				<SidebarLg />
+				<Navbar />
+				<Routes>
+					{/*------ Content Area ------*/}
+
+					<Route path="/" element={<Home />} />
+					<Route path="/trending" element={<Trending />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,6 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: "#7B2CBF",
 				secondary: "#FF6D00",
 			},
 			fontFamily: {
